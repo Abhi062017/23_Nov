2dc6af16c8e1b4d6309a802df81d3960f30c1ec6
Abhi062017 <abhi.waseian@gmail.com>
2017-11-22 20:13:03 +0530

    code script for C5.0 classification and UCI dataset file added

---
+++ C5.0 classification on UCI's 'balance-scale' dataset.R
@@ -0,0 +1,45 @@
+#C5.0 classification on UCI's 'balance-scale' dataset
+getwd()
+data <- read.csv('balance-scale.data', header = F, stringsAsFactors = F)
+str(data)
+summary(data)
+unique(data$V1)
+data$V1 <- as.factor(data$V1) #casting the response variable into factor
+
+#randomize for stratified split
+random.data <- data[order(runif(nrow(data))),]
+View(random.data)
+
+#split
+nrow(random.data)
+0.8*625
+train <- random.data[1:500,]
+test <- random.data[501:625,]
+#***************************************************************************
+
+#C5.0 model building
+install.packages('C50', dependencies = T)
+library(C50)
+model <- C5.0(random.data[,-1], random.data[,1])
+summary(model)
+plot(model)
+
+#prediction
+model.pred <- predict(model, test)
+model.pred
+table(test[,1], model.pred) #confusion matrix of sorts
+sum(diag(table(test[,1], model.pred)))/sum(table(test[,1], model.pred)) #accuracy
+#*****************************************************************************
+
+#Tuning:
+#Tune Model by introducing 'trials' (iterations)
+model.tuned <- C5.0(random.data[,-1], random.data[,1], trials = 25)
+model.tuned
+plot(model.tuned)
+
+#prediciton
+model.tuned.pred <- predict(model.tuned, test)
+model.tuned.pred
+table(values = test[,1],model.tuned.pred) #confusion matrix of sorts
+a <- table(values = test[,1],model.tuned.pred)
+sum(diag(a))/sum(a) #accuracy (better than the one from previous model)
